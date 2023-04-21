9992e547c37bfba210193ea4a2467eedc7163a2d
DM2735 <quseis@naver.com>
2023-04-21 17:40:13 +0900

    회원가입 유효성 검사 완료 User까지 등록가능

--- src/main/java/com/toyproject/bookmanagement/security/JwtTokenProvider.java
+++ src/main/java/com/toyproject/bookmanagement/security/JwtTokenProvider.java
@@ -6,13 +6,18 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.toyproject.bookmanagement.dto.auth.JwtTokenRespDto;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -52,5 +57,36 @@ public class JwtTokenProvider {
 				.grantType("Bearer")
 				.accessToken(accessToken)
 				.build();
+	}
+	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(key)
+				.build()
+				.parseClaimsJws(token);
+			
+			return true;
+		} catch (SecurityException | MalformedJwtException e) {
+			log.info("Invalid JWT Token", e);
+		} catch (ExpiredJwtException e) {
+			log.info("Expired JWT Token", e);
+		} catch (UnsupportedJwtException e) {
+			log.info("Unsupported JWT Token", e);
+		} catch (IllegalArgumentException e) {
+			log.info("IllegalArgument JWT Token", e);
+		} catch (Exception e) {
+			log.info("JWT Token Error", e);
+		}
+		
+		return false;
+	}
+	
+	public String getToken(String token) {
+		String type = "Bearer";
+		if(StringUtils.hasText(token) && token.startsWith(type)) {
+			return token.substring(type.length() + 1);
+		}
+		return null;
 	}
 }
